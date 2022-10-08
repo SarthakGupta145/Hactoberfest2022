5e34135e1d8efcbdecb9415f9ba9a5e2a9e45b56
Sarthak Gupta <87241485+SarthakGupta145@users.noreply.github.com>
2022-10-09 00:02:54 +0530

    Update InsertionSort.c
    syntax changes

--- C/Sorting/InsertionSort.c
+++ C/Sorting/InsertionSort.c
@@ -3,11 +3,11 @@ int main(){
 
    int i, j, count, temp, number[25];
 
-   printf("How many numbers u are going to enter?: ");
+   printf("How many numbers u are wishing to enter?: ");
    scanf("%d",&count);
 
    printf("Enter %d elements: ", count);
-   // This loop would store the input numbers in array
+   // This loop would store the input numbers in array .
    for(i=0;i<count;i++)
       scanf("%d",&number[i]);
 
